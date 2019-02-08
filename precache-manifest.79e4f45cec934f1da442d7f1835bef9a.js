2ee970f6bf38d3b94c3b654def86f7e9b2e17193
IMUSICA\richard.quintanilha <richard.quintanilha@imusica.com.br>
2019-02-08 09:58:52 -0200

    Updates

---
+++ precache-manifest.79e4f45cec934f1da442d7f1835bef9a.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = [
+  {
+    "revision": "d7d5d4588a9f50c99264bc12e4892a7c",
+    "url": "/cavacoweb/static/media/OpenSans-Regular.d7d5d458.ttf"
+  },
+  {
+    "revision": "674e750fb5a62bad89fe",
+    "url": "/cavacoweb/static/css/main.22fe135a.chunk.css"
+  },
+  {
+    "revision": "9ec7687a405240a13854",
+    "url": "/cavacoweb/static/js/1.9ec7687a.chunk.js"
+  },
+  {
+    "revision": "2eeda7b06f358370c562",
+    "url": "/cavacoweb/static/js/runtime~main.2eeda7b0.js"
+  },
+  {
+    "revision": "7d3779b19f5b09d292236cc3afec5d2a",
+    "url": "/cavacoweb/static/media/OpenSans-Bold.7d3779b1.woff2"
+  },
+  {
+    "revision": "f9fc9780feed3b7444ba6277f067c7f5",
+    "url": "/cavacoweb/static/media/OpenSans-Bold.f9fc9780.woff"
+  },
+  {
+    "revision": "691f774319fda1ccf9ea172ffaee815e",
+    "url": "/cavacoweb/static/media/OpenSans-Light.691f7743.woff2"
+  },
+  {
+    "revision": "804037562eabaa5dbefa4a435206d017",
+    "url": "/cavacoweb/static/media/OpenSans-Light.80403756.eot"
+  },
+  {
+    "revision": "edab3663655deaddbc4baf33e83838dd",
+    "url": "/cavacoweb/static/media/OpenSans-Light.edab3663.woff"
+  },
+  {
+    "revision": "9ff12f694e5951a6f51a9d63b05062e7",
+    "url": "/cavacoweb/static/media/OpenSans-Light.9ff12f69.ttf"
+  },
+  {
+    "revision": "a35546eef3ea0de0d4735f23b0a0873b",
+    "url": "/cavacoweb/static/media/OpenSans-Regular.a35546ee.eot"
+  },
+  {
+    "revision": "b22164d568f72650d592271d22dd5cda",
+    "url": "/cavacoweb/static/media/OpenSans-Light.b22164d5.svg"
+  },
+  {
+    "revision": "e78dce533ecee30c5efd812bb23c248d",
+    "url": "/cavacoweb/static/media/OpenSans-Regular.e78dce53.woff2"
+  },
+  {
+    "revision": "552ea4cf95b85269f5a3b5b1e2b2eedb",
+    "url": "/cavacoweb/static/media/OpenSans-Regular.552ea4cf.woff"
+  },
+  {
+    "revision": "0ea04502930623aa3de110aac9f9e4fd",
+    "url": "/cavacoweb/static/media/OpenSans-Semibold.0ea04502.eot"
+  },
+  {
+    "revision": "674e750fb5a62bad89fe",
+    "url": "/cavacoweb/static/js/main.674e750f.chunk.js"
+  },
+  {
+    "revision": "bf4dde0147bb02556500d922e2efc90b",
+    "url": "/cavacoweb/static/media/OpenSans-Regular.bf4dde01.svg"
+  },
+  {
+    "revision": "e9681ca3d29d814a5621d4764dd1a11e",
+    "url": "/cavacoweb/static/media/OpenSans-Semibold.e9681ca3.woff2"
+  },
+  {
+    "revision": "1d8cbd42230b32f36e03e5fa9cfc27bc",
+    "url": "/cavacoweb/static/media/OpenSans-Semibold.1d8cbd42.woff"
+  },
+  {
+    "revision": "caba68b9c5aa72a1a9729df5e1e490d2",
+    "url": "/cavacoweb/static/media/OpenSans-Bold.caba68b9.eot"
+  },
+  {
+    "revision": "1356004021c35e279adc37c22f66a619",
+    "url": "/cavacoweb/static/media/OpenSans-Bold.13560040.ttf"
+  },
+  {
+    "revision": "e1c83f9474e0cc1d84a13c6d1ddf3ca5",
+    "url": "/cavacoweb/static/media/OpenSans-Semibold.e1c83f94.ttf"
+  },
+  {
+    "revision": "3545dcf2f989141a276e4042ab08e646",
+    "url": "/cavacoweb/static/media/OpenSans-ExtraBold.3545dcf2.eot"
+  },
+  {
+    "revision": "cdd3182d232d67c0ea2993da79e790d8",
+    "url": "/cavacoweb/static/media/OpenSans-Bold.cdd3182d.svg"
+  },
+  {
+    "revision": "00ded89f00516dc8b48543af7ebe41b9",
+    "url": "/cavacoweb/static/media/OpenSans-ExtraBold.00ded89f.woff2"
+  },
+  {
+    "revision": "cea262c1bd6327c10f25e75d1d98f8dc",
+    "url": "/cavacoweb/static/media/OpenSans-ExtraBold.cea262c1.woff"
+  },
+  {
+    "revision": "9a5e270b2723d071eb17656bd5bc6d5f",
+    "url": "/cavacoweb/static/media/OpenSans-ExtraBold.9a5e270b.ttf"
+  },
+  {
+    "revision": "bfe65cb23f73e1b51f2040f185a8afb2",
+    "url": "/cavacoweb/static/media/OpenSans-ExtraBold.bfe65cb2.svg"
+  },
+  {
+    "revision": "24aab533f87e7b434be5fa5b1684975c",
+    "url": "/cavacoweb/static/media/OpenSans-Semibold.24aab533.svg"
+  },
+  {
+    "revision": "c3ebc1493339bc77501d68e1c083a500",
+    "url": "/cavacoweb/index.html"
+  }
+];
